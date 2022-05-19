d7abe6e38150d719ddf96a365d587b6b9a76b1a2
laurensruiz <laurenruiz97@yahoo.com>
2022-05-19 15:20:24 -0400

    started index and server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express');
+
+//const RecipesRouter = require('./recipes/recipes-router');
+
+const server = express();
+
+server.use(express.json());
+
+
+//server.use('/api/recipes', RecipesRouter);
+
+//catchall
+/*server.use('*', (req, res) => {
+    res.json({api:'up'})
+})*/
+
+module.exports = server;
